51364dc495be324dc2bcf488e7a2b35b267da0bd
Benjamin Lister <48570319+007spy9@users.noreply.github.com>
2024-07-15 22:01:06 +0100

    Added basicESN for HAR70+ and Hierarchical ESN

--- model/reservoir/BasicESN.py
+++ model/reservoir/BasicESN.py
@@ -2,10 +2,11 @@ import tensorflow as tf
 import numpy as np
 import sklearn as sk
 from sklearn.linear_model import Ridge
+from tqdm import tqdm
 
 class BasicESN:
     def __init__(self, leakage_rate, spectral_radius, gamma, n_neurons, W_in):
-        self.leakage_rate = tf.Variable(leakage_rate, trainable=False, dtype=tf.float32)
+        self.leakage_rate = leakage_rate
         self.spectral_radius = spectral_radius
         self.gamma = gamma
         self.N = n_neurons
@@ -29,17 +30,22 @@ class BasicESN:
         self.previous_states = []
         state = np.zeros((self.N, 1))
 
-        for i in range(x.shape[0]):
-            # Compute the reservoir state using the equation
-            # h(t+1) = h(t) * leakage_rate  + (1 - leakage_rate) * tanh((gamma * W_in * x(t)) + (spectral_radius * W_res * h(t)) + bias)
-            non_linear = np.tanh((self.gamma * self.W_in * x[i]) + (self.spectral_radius * self.W_res @ state))
+        with tqdm(total=x.shape[0]) as pbar:
+            for i in range(x.shape[0]):
+                # Compute the reservoir state using the equation
+                # h(t+1) = h(t) * leakage_rate  + (1 - leakage_rate) * tanh((gamma * W_in * x(t)) + (spectral_radius * W_res * h(t)) + bias)
+                non_linear = np.tanh((self.gamma * self.W_in @ x[i].reshape(-1, 1)) + (self.spectral_radius * self.W_res @ state))
 
-            state = (state * self.leakage_rate) + ((1 - self.leakage_rate) * non_linear)
+                state = (state * self.leakage_rate) + ((1 - self.leakage_rate) * non_linear)
 
-            self.previous_states.append(state)
+                # Append the state after ravel
+                self.previous_states.append(state.ravel())
+
+                pbar.update(1)
 
         # Flatten the previous_states list to a 2D array of shape (n_samples, n_neurons)
-        self.previous_states = np.array(self.previous_states).reshape(x.shape[0], self.N)
+        self.previous_states = np.array(self.previous_states)
+        print(f"Shape of previous_states: {self.previous_states.shape}")
 
         return self.previous_states
 
@@ -53,6 +59,8 @@ class BasicESN:
         # Compute the output from the readout layer
         y = self.readout.predict(state)
 
+        #y_pred = np.argmax(y, axis=1)
+
         return y
 
     def get_state_history(self):
@@ -61,6 +69,9 @@ class BasicESN:
     def fit(self, x, y):
         # Compute the reservoir state
         state = self.compute_reservoir_state(x)
+
+        print(f"Shape of state before fitting: {state.shape}")
+        print(f"Shape of y before fitting: {y.shape}")
 
         # Fit the readout layer
         # TODO: Add class weights
--- model/reservoir/HierarchyESN.py
+++ model/reservoir/HierarchyESN.py
@@ -2,44 +2,80 @@ import tensorflow as tf
 import numpy as np
 import sklearn as sk
 from sklearn.linear_model import Ridge
+from tqdm import tqdm
 
 class HierarchyESN:
-    def __init__(self, leakage_rate, spectral_radius, gamma, n_neurons, W_in):
-        self.leakage_rate = tf.Variable(leakage_rate, trainable=False, dtype=tf.float32)
-        self.spectral_radius = spectral_radius
-        self.gamma = gamma
-        self.N = n_neurons
-        self.W_in = W_in
+    def __init__(self, leakage_rate_1, spectral_radius_1, gamma_1, n_neurons_1, W_in_1, leakage_rate_2, spectral_radius_2, gamma_2, n_neurons_2, W_in_2):
+        self.leakage_rate_1 = leakage_rate_1
+        self.spectral_radius_1 = spectral_radius_1
+        self.gamma_1 = gamma_1
+        self.N_1 = n_neurons_1
+        self.W_in_1 = W_in_1
 
-        self.W_res = np.random.uniform(-1, 1, (self.N, self.N))
+        self.W_res_1 = np.random.uniform(-1, 1, (self.N_1, self.N_1))
 
-        res_eigenvalues = np.linalg.eigvals(self.W_res)
+        res_eigenvalues_1 = np.linalg.eigvals(self.W_res_1)
 
         # Scale the reservoir weights
-        self.W_res /= np.max(np.abs(res_eigenvalues))
+        self.W_res_1 /= np.max(np.abs(res_eigenvalues_1))
 
-        # To begin with, the readout layer is a Ridge from sklearn
+        self.leakage_rate_2 = leakage_rate_2
+        self.spectral_radius_2 = spectral_radius_2
+        self.gamma_2 = gamma_2
+        self.N_2 = n_neurons_2
+        self.W_in_2 = W_in_2
+
+        self.W_res_2 = np.random.uniform(-1, 1, (self.N_2, self.N_2))
+
+        res_eigenvalues_2 = np.linalg.eigvals(self.W_res_2)
+
+        # Scale the reservoir weights
+        self.W_res_2 /= np.max(np.abs(res_eigenvalues_2))
+
+        # Now, we also need to prepare the connectivity matrix between the two reservoirs
+        # This matrix will be of shape (n_neurons_1, n_neurons_2), and will be randomly initialized from a uniform distribution
+        self.W_12 = np.random.uniform(-1, 1, (self.N_1, self.N_2))
+
+        # Again, we need to scale the connectivity matrix
+        self.W_12 /= np.max(np.abs(np.linalg.eigvals(self.W_12)))
+
+        # We will use another ridge regression model for the readout layer
         self.readout = Ridge(alpha=1.0)
-
-        self.previous_states = []
 
     def compute_reservoir_state(self, x):
         # Initialize the reservoir state
         # Define a previous_states list to store the state of the reservoir at each time step
         self.previous_states = []
-        state = np.zeros((self.N, 1))
+        state1 = np.zeros((self.N_1, 1))
+        state2 = np.zeros((self.N_2, 1))
+        state = np.zeros((self.N_1 + self.N_2, 1))
 
-        for i in range(x.shape[0]):
-            # Compute the reservoir state using the equation
-            # h(t+1) = h(t) * leakage_rate  + (1 - leakage_rate) * tanh((gamma * W_in * x(t)) + (spectral_radius * W_res * h(t)) + bias)
-            non_linear = np.tanh((self.gamma * self.W_in * x[i]) + (self.spectral_radius * self.W_res @ state))
+        with tqdm(total=x.shape[0]) as pbar:
+            for i in range(x.shape[0]):
+                # Compute the reservoir state. As this is a hierarchical ESN, we will have two reservoirs
+                # The hidden state of the first reservoir will be computed as follows:
+                # h_1(t+1) = h_1(t) * leakage_rate_1  + (1 - leakage_rate_1) * tanh((gamma_1 * W_in_1 * x(t)) + (spectral_radius_1 * W_res_1 * h_1(t)) + bias_1)
 
-            state = (state * self.leakage_rate) + ((1 - self.leakage_rate) * non_linear)
+                # The hidden state of the second reservoir will also include components of the first reservoir, defined through the connectivity matrix W_12
+                # h_2(t+1) = h_2(t) * leakage_rate_2  + (1 - leakage_rate_2) * tanh((gamma_2 * W_in_2 * x(t)) + (spectral_radius_2 * W_res_2 * h_2(t)) + (W_12 * h_1(t)) + bias_2)
 
-            self.previous_states.append(state)
+                non_linear_1 = np.tanh((self.gamma_1 * self.W_in_1 @ x[i].reshape(-1, 1)) + (self.spectral_radius_1 * self.W_res_1 @ state1))
+                non_linear_2 = np.tanh((self.gamma_2 * self.W_in_2 @ x[i].reshape(-1, 1)) + (self.spectral_radius_2 * self.W_res_2 @ state2) + (self.W_12 @ state1))
+
+                state_1 = (state1 * self.leakage_rate_1) + ((1 - self.leakage_rate_1) * non_linear_1)
+                state_2 = (state2 * self.leakage_rate_2) + ((1 - self.leakage_rate_2) * non_linear_2)
+
+                # The state of the second reservoir will be the output of the model concatenated with the state of the first reservoir
+                state = np.concatenate((state_1, state_2), axis=0)
+
+                # Append the state after ravel
+                self.previous_states.append(state.ravel())
+
+                pbar.update(1)
 
         # Flatten the previous_states list to a 2D array of shape (n_samples, n_neurons)
-        self.previous_states = np.array(self.previous_states).reshape(x.shape[0], self.N)
+        self.previous_states = np.array(self.previous_states)
+        print(f"Shape of previous_states: {self.previous_states.shape}")
 
         return self.previous_states
 
@@ -53,6 +89,8 @@ class HierarchyESN:
         # Compute the output from the readout layer
         y = self.readout.predict(state)
 
+        #y_pred = np.argmax(y, axis=1)
+
         return y
 
     def get_state_history(self):
@@ -61,6 +99,9 @@ class HierarchyESN:
     def fit(self, x, y):
         # Compute the reservoir state
         state = self.compute_reservoir_state(x)
+
+        print(f"Shape of state before fitting: {state.shape}")
+        print(f"Shape of y before fitting: {y.shape}")
 
         # Fit the readout layer
         # TODO: Add class weights
